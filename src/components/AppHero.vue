7f9eef7cd72565a1aab8befcf2f9bbf1c0481c96
GiuliaGlave <giulia.glave@gmail.com>
2023-02-26 18:50:07 +0100

    title + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div>
+    <img
+      src="https://images.bauerhosting.com/legacy/media/6282/805b/d89b/4873/b676/2b93/boys-season-3-1.jpg?q=80&w=1400&ar=16:9&fit=crop&crop=top"
+      alt=""
+    />
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 70%;
+}
+</style>
